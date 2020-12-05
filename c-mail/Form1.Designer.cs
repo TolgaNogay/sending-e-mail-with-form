1c2ec3cee2f3656fd665f2e94988379397352b5e
Tolga Nogay <47275502+TolgaNogay@users.noreply.github.com>
2020-12-05 13:10:24 +0300

    Content added.
    The content was uploaded without any problems.

---
+++ c-mail/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace c_mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.textBoxBaslik = new System.Windows.Forms.TextBox();
+            this.textBoxİcerik = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Helvetica Neue", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(114, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kime: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Başlık: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mesaj İçeriği: ";
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(114, 61);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(143, 20);
+            this.textBoxMail.TabIndex = 4;
+            // 
+            // textBoxBaslik
+            // 
+            this.textBoxBaslik.Location = new System.Drawing.Point(114, 87);
+            this.textBoxBaslik.Name = "textBoxBaslik";
+            this.textBoxBaslik.Size = new System.Drawing.Size(143, 20);
+            this.textBoxBaslik.TabIndex = 5;
+            // 
+            // textBoxİcerik
+            // 
+            this.textBoxİcerik.Location = new System.Drawing.Point(114, 113);
+            this.textBoxİcerik.Multiline = true;
+            this.textBoxİcerik.Name = "textBoxİcerik";
+            this.textBoxİcerik.Size = new System.Drawing.Size(143, 113);
+            this.textBoxİcerik.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 311);
+            this.Controls.Add(this.textBoxİcerik);
+            this.Controls.Add(this.textBoxBaslik);
+            this.Controls.Add(this.textBoxMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mail Gönderme Programı Örneği";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.TextBox textBoxBaslik;
+        private System.Windows.Forms.TextBox textBoxİcerik;
+    }
+}
+
---
+++ c-mail/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace c_mail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+ _        _                                           
+| |      | |                                          
+| |_ ___ | | __ _  __ _ _ __   ___   __ _  __ _ _   _ 
+| __/ _ \| |/ _` |/ _` | '_ \ / _ \ / _` |/ _` | | | |
+| || (_) | | (_| | (_| | | | | (_) | (_| | (_| | |_| |
+ \__\___/|_|\__, |\__,_|_| |_|\___/ \__, |\__,_|\__, |
+             __/ |                   __/ |       __/ |
+            |___/                   |___/       |___/ 
+        
+        */
+
+        /*
+                             
+                     
+  ___ ___  _ __ ___  
+ / __/ _ \| '_ ` _ \ 
+| (_| (_) | | | | | |
+ \___\___/|_| |_| |_|
+                     
+                    
+
+
+        */
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MailMessage mesajim = new MailMessage();
+            SmtpClient istemci = new SmtpClient();
+
+            istemci.Credentials = new System.Net.NetworkCredential("edit@tolganogay.com", "password");
+            istemci.Port = 465;
+            istemci.Host = "mail.tolganogay.com";
+            istemci.EnableSsl = true;
+            mesajim.To.Add(textBoxMail.Text);
+            mesajim.From = new MailAddress("edit@tolganogay.com");
+            mesajim.Subject = textBoxBaslik.Text;
+            mesajim.Body = textBoxİcerik.Text;
+            istemci.Send(mesajim);
+
+            // Bu içerik 𝐓𝐨𝐥𝐠𝐚 𝐍𝐨𝐠𝐚𝐲 (tolganogay.com / github.com/TolgaNogay) tarafından oluşturulmuştur. 
+            // Kopyalanması yasaktır.
+
+            // This content was created by 𝐓𝐨𝐥𝐠𝐚 𝐍𝐨𝐠𝐚𝐲 (tolganogay.com / github.com/TolgaNogay).
+            // Copying is prohibited.
+
+
+        }
+    }
+}
